bd5c239e6d25d99ca6105c88f529707d8f4693a2
MadNessMusic <contact@madnessmusicgroup.com>
2026-04-26 22:30:22 -0600

    Fix vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel/server';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output:'server',
+  output: 'server',
 
   vite: {
     plugins: [tailwindcss()]
